72ca745cf36c40d6fd2713faba0aae3c0f8772cb
Marco-Nagy <marconbishay@gmail.com>
2024-05-08 18:20:05 +0300

    initial shoply project

---
+++ android/app/src/main/kotlin/com/marcosoft/shoply/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.marcosoft.shoply
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:shoply/soply_app.dart';
+
+void main() {
+  runApp(const ShoplyApp());
+}
