103f59fc09d52a30f85d65ef91239a623d24482d
Marco-Nagy <marconbishay@gmail.com>
2024-05-14 23:20:38 +0300

    fix FirebaseOptions variables

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,5 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EnvVariables.instance.init(envType: EnvTypeEnum.dev);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  
   runApp(const ShoplyApp());
 }
