552747261e547a88a872e8dd338f0d7b571926cd
Marco Nagy <71529412+Marco-Nagy@users.noreply.github.com>
2024-06-01 19:56:05 +0300

    add App Routes
    add App Routes
    and Navigation Animation
    add extensions

---
+++ lib/page_two.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shoply/core/helpers/extentions.dart';
+import 'package:shoply/core/styles/my_fonts.dart';
+
+class PageTwo extends StatelessWidget {
+  const PageTwo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amberAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.amberAccent,
+        title: Text(
+          'بيتر ناجى',
+          style: MyFonts.arabicBold700_18,),),
+      body:  InkWell(
+        onTap: (){
+          context.pop();
+        },
+        child: Center(child:        Text(
+          'Peter Nagy',
+          style: MyFonts.englishMedium500_16,
+        ),),
+      ),
+    );
+  }
+}
