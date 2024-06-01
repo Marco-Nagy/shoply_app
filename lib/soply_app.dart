e81eb2183c41ff934593bae47d864eab692deb23
Marco Nagy <71529412+Marco-Nagy@users.noreply.github.com>
2024-06-01 19:56:05 +0300

    add App Routes
    add App Routes
    and Navigation Animation
    add extensions

--- lib/soply_app.dart
+++ lib/soply_app.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shoply/core/app/connectivity_controller.dart';
 import 'package:shoply/core/app/env_variables.dart';
+import 'package:shoply/core/routes/app_routes.dart';
+import 'package:shoply/core/styles/my_fonts.dart';
 import 'package:shoply/core/utils/screens/no_network_screen.dart';
 
 class ShoplyApp extends StatelessWidget {
@@ -10,34 +14,38 @@ class ShoplyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
-        valueListenable: ConnectivityController.instance.isConnected,
-        builder: (_, value, __) {
-          if (value) {
-            return MaterialApp(
-            title: 'Shoply App',
-            debugShowCheckedModeBanner: EnvVariables.instance.debugMode,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
-            ),
-            builder: (context, child) => Scaffold(body: Builder(builder: (context) {
-              ConnectivityController.instance.init();
-              return child!;
-            },),),
-            home: Scaffold(
-              appBar: AppBar(title: const Text('Shoply App')),
+      valueListenable: ConnectivityController.instance.isConnected,
+      builder: (_, value, __) {
+        if (value) {
+          return ScreenUtilInit(
+            designSize: const Size(390, 844),
+            child: MaterialApp(
+              title: 'Shoply App',
+              debugShowCheckedModeBanner: EnvVariables.instance.debugMode,
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              builder: (context, child) => Scaffold(
+                body: Builder(
+                  builder: (context) {
+                    ConnectivityController.instance.init();
+                    return child!;
+                  },
+                ),
+              ),
+              initialRoute: '/',
+              onGenerateRoute: AppRoutes.onGenerateRoute,
             ),
           );
-        }else{
-            return   MaterialApp(
-              title: 'No Network',
-              debugShowCheckedModeBanner: EnvVariables.instance.debugMode,
-              home: const NoNetworkScreen(),
-            );
-          }
-
-        },);
-
-
+        } else {
+          return MaterialApp(
+            title: 'No Network',
+            debugShowCheckedModeBanner: EnvVariables.instance.debugMode,
+            home: const NoNetworkScreen(),
+          );
+        }
+      },
+    );
   }
 }
