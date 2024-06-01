2927647572af4af910c5f35db13e89fa2ca42763
Marco Nagy <71529412+Marco-Nagy@users.noreply.github.com>
2024-06-01 20:50:27 +0300

    Reapply "Routes"
    This reverts commit 2c9b68fe485c043a8655e3dae6a7d53eaf987aa4.

---
+++ lib/core/styles/my_fonts.dart
@@ -0,0 +1,126 @@
+import 'dart:ui';
+
+import 'package:flutter/src/painting/text_style.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyFonts {
+
+  // Arabic Text Styles
+  static TextStyle arabicRegular400_12 = GoogleFonts.cairo(
+    fontWeight: FontWeight.w400,
+    fontSize: 12.sp,
+  );
+
+  static TextStyle arabicRegular400_14 = GoogleFonts.cairo(
+    fontWeight: FontWeight.w400,
+    fontSize: 14.sp,
+  );
+
+  static TextStyle arabicRegular400_16 = GoogleFonts.cairo(
+    fontWeight: FontWeight.w400,
+    fontSize: 16.sp,
+  );
+
+  static TextStyle arabicRegular400_18 = GoogleFonts.cairo(
+    fontWeight: FontWeight.w400,
+    fontSize: 18.sp,
+  );
+
+  static TextStyle arabicMedium500_12 = GoogleFonts.cairo(
+    fontWeight: FontWeight.w500,
+    fontSize: 12.sp,
+  );
+
+  static TextStyle arabicMedium500_14 = GoogleFonts.cairo(
+    fontWeight: FontWeight.w500,
+    fontSize: 14.sp,
+  );
+
+  static TextStyle arabicMedium500_16 = GoogleFonts.cairo(
+    fontWeight: FontWeight.w500,
+    fontSize: 16.sp,
+  );
+
+  static TextStyle arabicMedium500_18 = GoogleFonts.cairo(
+    fontWeight: FontWeight.w500,
+    fontSize: 18.sp,
+  );
+
+  static TextStyle arabicSemiBold600_12 = GoogleFonts.cairo(
+    fontWeight: FontWeight.w600,
+    fontSize: 12.sp,
+  );
+
+  static TextStyle arabicSemiBold600_14 = GoogleFonts.cairo(
+    fontWeight: FontWeight.w600,
+    fontSize: 14.sp,
+  );
+
+  static TextStyle arabicSemiBold600_16 = GoogleFonts.cairo(
+    fontWeight: FontWeight.w600,
+    fontSize: 16.sp,
+  );
+
+  static TextStyle arabicSemiBold600_18 = GoogleFonts.cairo(
+    fontWeight: FontWeight.w600,
+    fontSize: 18.sp,
+  );
+
+  static TextStyle arabicBold700_12 = GoogleFonts.cairo(
+    fontWeight: FontWeight.w700,
+    fontSize: 12.sp,
+  );
+
+  static TextStyle arabicBold700_14 = GoogleFonts.cairo(
+    fontWeight: FontWeight.w700,
+    fontSize: 14.sp,
+  );
+
+  static TextStyle arabicBold700_16 = GoogleFonts.cairo(
+    fontWeight: FontWeight.w700,
+    fontSize: 16.sp,
+  );
+
+  static TextStyle arabicBold700_18 = GoogleFonts.cairo(
+    fontWeight: FontWeight.w700,
+    fontSize: 18.sp,
+  );
+
+  // English Text Styles
+  static TextStyle englishRegular400_12 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w400,
+    fontSize: 12.sp,
+  );
+
+  static TextStyle englishRegular400_14 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w400,
+    fontSize: 14.sp,
+  );
+
+  static TextStyle englishRegular400_16 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w400,
+    fontSize: 16.sp,
+  );
+
+  static TextStyle englishRegular400_18 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w400,
+    fontSize: 18.sp,
+  );
+
+  static TextStyle englishMedium500_12 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w500,
+    fontSize: 12.sp,
+  );
+
+  static TextStyle englishMedium500_14 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w500,
+    fontSize: 14.sp,
+  );
+
+  static TextStyle englishMedium500_16 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w500,
+    fontSize: 16.sp,
+  );
+
+}
