88d248d6bfbb0e918f0b6d8deaa2878ce45bb875
Marco Nagy <71529412+Marco-Nagy@users.noreply.github.com>
2024-06-11 10:39:18 +0300

    initial shoply project

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shoply/soply_app.dart';
 
 Future<void> main() async {
+  
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const ShoplyApp());
 }
