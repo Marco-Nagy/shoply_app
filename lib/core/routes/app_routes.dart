5a32f4421be60f0c36670d4054ffc1dd3d9bf1e9
Marco Nagy <71529412+Marco-Nagy@users.noreply.github.com>
2024-06-11 09:20:53 +0300

    create sign in UI design

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:shoply/core/routes/base_routes.dart';
 import 'package:shoply/core/utils/screens/under_build_screen.dart';
-import 'package:shoply/page_one.dart';
+import 'package:shoply/features/auth/presentation/screens/login_screen.dart';
 import 'package:shoply/page_two.dart';
 
 class AppRoutes {
-  static const String one = '/';
+  static const String login = '/';
   static const String two = 'two';
   static Route<void> onGenerateRoute(RouteSettings settings){
     switch (settings.name) {
-      case AppRoutes.one:
-        return BaseRoute(page:  const PageOne());
+      case AppRoutes.login:
+        return BaseRoute(page:  const LoginScreen());
       case AppRoutes.two:
         return  BaseRoute(page: const PageTwo());
       default:
