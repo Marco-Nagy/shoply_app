ce4db2687ef981824a251d999b8fe53ef7e62790
Marco Nagy <71529412+Marco-Nagy@users.noreply.github.com>
2024-06-18 13:44:29 +0300

    Update Login data Layer
    Handle Auth Repository with Auth Bloc
    Handle Auth Bloc functions
     Events , Bloc , States

--- lib/core/app/apis/api_service.g.dart
+++ lib/core/app/apis/api_service.g.dart
@@ -49,21 +49,20 @@ class _ApiService implements ApiService {
   }
 
   @override
-  Future<UserRoleResponse> userRole(Map<String, dynamic> query) async {
+  Future<UserRoleResponse> userRole() async {
     final _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
     final _headers = <String, dynamic>{};
-    final _data = <String, dynamic>{};
-    _data.addAll(query);
+    const Map<String, dynamic>? _data = null;
     final _result = await _dio
         .fetch<Map<String, dynamic>>(_setStreamType<UserRoleResponse>(Options(
-      method: 'POST',
+      method: 'GET',
       headers: _headers,
       extra: _extra,
     )
             .compose(
               _dio.options,
-              '/graphql',
+              '/api/v1/auth/profile',
               queryParameters: queryParameters,
               data: _data,
             )
--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shoply/core/app/service_locator/injection_container.dart';
 import 'package:shoply/core/routes/base_routes.dart';
 import 'package:shoply/core/utils/screens/under_build_screen.dart';
+import 'package:shoply/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:shoply/features/auth/presentation/screens/login_screen.dart';
 import 'package:shoply/features/auth/presentation/screens/sign_up_screen.dart';
 
 class AppRoutes {
   static const String login = '/';
   static const String signUp = 'signUp';
-  static Route<void> onGenerateRoute(RouteSettings settings){
+
+  static Route<void> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case AppRoutes.login:
-        return BaseRoute(page:  const LoginScreen());
+        return BaseRoute(page: BlocProvider(
+          create: (context) => sl<AuthBloc>(),
+          child: const LoginScreen(),
+        ));
       case AppRoutes.signUp:
-        return  BaseRoute(page: const SignUpScreen());
+        return BaseRoute(page: const SignUpScreen());
       default:
         return BaseRoute(page: const PageUnderBuildScreen());
     }
