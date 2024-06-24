b3748bf02085da6b1972bcd3a01709b91ec9c1b5
Marco Nagy <71529412+Marco-Nagy@users.noreply.github.com>
2024-06-25 02:32:08 +0300

    Add Development and Production flavors for Android
    - added Development and Production flavors for Android
    - changed app name for each flavor
    - handle flavor configurations for android studio

---
+++ lib/main_development.dart
@@ -0,0 +1,50 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shoply/core/Services/shared_preference/shared_preference_helper.dart';
+import 'package:shoply/core/app/abb_bloc_observer.dart';
+import 'package:shoply/core/app/env_variables.dart';
+import 'package:shoply/core/app/service_locator/injection_container.dart';
+import 'package:shoply/firebase_options.dart';
+import 'package:shoply/soply_app.dart';
+import 'package:flutter/material.dart';
+
+import 'app_config.dart';
+
+
+
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await EnvVariables.instance.init(envType: EnvTypeEnum.dev);
+//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+//   await SharedPrefHelper().instantiatePreferences();
+//   await setupInjector();
+//   Bloc.observer = MyBlocObserver();
+//
+//   runApp(const ShoplyApp());
+// }
+
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EnvVariables.instance.init(envType: EnvTypeEnum.dev);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await SharedPrefHelper().instantiatePreferences();
+  await setupInjector();
+  Bloc.observer = MyBlocObserver();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.green,
+    systemNavigationBarColor: Colors.green,
+  ));
+  var appConfig = AppConfig(
+    appEnvironment: EnvTypeEnum.dev,
+    appName: 'Shoply  Development',
+    description: 'This is a Development version of Shoply',
+    baseUrl: '',
+    child: const ShoplyApp(),
+  );
+  runApp(appConfig);
+}
