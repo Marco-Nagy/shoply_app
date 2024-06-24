7e0a2aa2a18c6f65d84446feeb5fa3331152728b
Marco Nagy <71529412+Marco-Nagy@users.noreply.github.com>
2024-06-25 02:32:08 +0300

    Add Development and Production flavors for Android
    - added Development and Production flavors for Android
    - changed app name for each flavor
    - handle flavor configurations for android studio

---
+++ lib/app_config.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:meta/meta.dart';
+import 'package:shoply/core/app/env_variables.dart';
+
+
+class AppConfig extends InheritedWidget {
+   AppConfig({
+    required this.appEnvironment,
+    required this.appName,
+    required this.description,
+    required this.baseUrl,
+    required this.child,
+    super.key,
+  }) : super(child: child);
+  final EnvTypeEnum appEnvironment;
+  final String appName;
+  final String description;
+  final String baseUrl;
+  @override
+  final Widget child;
+
+  static AppConfig? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType();
+  }
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) => false;
+}
